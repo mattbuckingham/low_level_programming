e18c039828d7bb269fb3d695a091ac94b7730c0b
bigBadMatt <5707@holbertonstudents.com>
2022-09-29 20:20:38 -0700

    fixed newline chars not working

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -14,10 +14,10 @@ int main(void)
 	long long int sLongLongInt;
 	float sFloat;
 
-	printf("Size of a char: %d byte(s)/n", sizeof(sChar));
-	printf("Size of an int: %d byte(s)/n", sizeof(sInt));
-	printf("Size of a long int: %d byte(s)/n", sizeof(sLongInt));
-	printf("Size of a long long inr: %d byte(s)/n", sizeof(sLongLongInt));
-	printf("Size of a float: %d byte(s)/n", sizeof(sFloat));
+	printf("Size of a char: %d byte(s)\n", sizeof(sChar));
+	printf("Size of an int: %d byte(s)\n", sizeof(sInt));
+	printf("Size of a long int: %d byte(s)\n", sizeof(sLongInt));
+	printf("Size of a long long inr: %d byte(s)\n", sizeof(sLongLongInt));
+	printf("Size of a float: %d byte(s)\n", sizeof(sFloat));
 	return (0);
 }

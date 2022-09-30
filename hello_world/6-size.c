89efb178fd75bffc6ab88ff98061b4e2c36ebd40
bigBadMatt <5707@holbertonstudents.com>
2022-09-29 20:18:53 -0700

    fixed wrong format specifier

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -11,13 +11,13 @@ int main(void)
 	char sChar;
 	int sInt;
 	long int sLongInt;
-	long long int sLongLongoInt;
+	long long int sLongLongInt;
 	float sFloat;
 
-	printf("Size of a char: %z byte(s)/n", sizeof(sChar));
-	printf("Size of an int: %z byte(s)/n", sizeof(sInt);
-	printf("Size of a long int: %z byte(s)/n", sizeof(sLongInt);
-	printf("Size of a long long inr: %z byte(s)/n", sizeof(sLongLongInt);
-	printf("Size of a float: %z byte(s)/n", sizeof(sFloat);
+	printf("Size of a char: %d byte(s)/n", sizeof(sChar));
+	printf("Size of an int: %d byte(s)/n", sizeof(sInt));
+	printf("Size of a long int: %d byte(s)/n", sizeof(sLongInt));
+	printf("Size of a long long inr: %d byte(s)/n", sizeof(sLongLongInt));
+	printf("Size of a float: %d byte(s)/n", sizeof(sFloat));
 	return (0);
 }

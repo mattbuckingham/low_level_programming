de5dd93ebc29f9281f429c6975df2de00c881629
bigBadMatt <5707@holbertonstudents.com>
2022-09-29 20:23:19 -0700

    typo in output

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -17,7 +17,7 @@ int main(void)
 	printf("Size of a char: %d byte(s)\n", sizeof(sChar));
 	printf("Size of an int: %d byte(s)\n", sizeof(sInt));
 	printf("Size of a long int: %d byte(s)\n", sizeof(sLongInt));
-	printf("Size of a long long inr: %d byte(s)\n", sizeof(sLongLongInt));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(sLongLongInt));
 	printf("Size of a float: %d byte(s)\n", sizeof(sFloat));
 	return (0);
 }

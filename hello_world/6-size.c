806da7b7d4ec266f4a4a3132d8ad6f654d68c610
bigBadMatt <5707@holbertonstudents.com>
2022-09-29 20:04:25 -0700

    fixed fatal error

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,10 +8,16 @@
 
 int main(void)
 {
-	printf("Size of a char: "sizeof(char)" byte(s)");
-	printf("Size of an int: "sizeof(int)" byte(s)");
-	printf("Size of a long int: "sizeof(long int)" byte(s)");
-	printf("Size of a long long int: "sizeof(long long int)" byte(s)");
-	printf("Size of a float: "sizeof(float)" byte(s)");
+	char sChar;
+	int sInt;
+	long int sLongInt;
+	long long int sLongLongoInt;
+	float sFloat;
+
+	printf("Size of a char: %z byte(s)/n", sizeof(sChar));
+	printf("Size of an int: %z byte(s)/n", sizeof(sInt);
+	printf("Size of a long int: %z byte(s)/n", sizeof(sLongInt);
+	printf("Size of a long long inr: %z byte(s)/n", sizeof(sLongLongInt);
+	printf("Size of a float: %z byte(s)/n", sizeof(sFloat);
 	return (0);
 }

907c98bc25c9ac656913f21b77b7d4c72cdaab32
bigBadMatt <5707@holbertonstudents.com>
2022-09-29 17:26:32 -0700

    function to print to output

---
+++ hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ *main - prints a string to output does not use the printf function
+ *
+ *Return: 0 at all time
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}

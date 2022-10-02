7fdcff801faf81eef82fa9b66ab639969e71e33c
bigBadMatt <5707@holbertonstudents.com>
2022-10-02 15:55:22 -0700

    added newline chars to output statements

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -16,10 +16,10 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	printf("%d", n);
 		if (n > 0)
-			printf(" is positive");
+			printf(" is positive\n");
 		else if (n == 0)
-			printf(" is zero");
+			printf(" is zero\n");
 		else if (n < 0)
-			printf(" is negative");
+			printf(" is negative\n");
 	return (0);
 }

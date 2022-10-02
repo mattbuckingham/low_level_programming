6ae309cff4ecf59be72c77aa4eb0135d9d48fc07
bigBadMatt <5707@holbertonstudents.com>
2022-10-02 15:49:33 -0700

    fixed missing sytax chars

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -14,14 +14,12 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf(n)
+	printf("%d", n);
 		if (n < 0)
-		{
 			printf("is positive");
 		else if (n == 0)
-			printf("is zero")
+			printf("is zero");
 		else if (n < 0)
-			printf("is negative")
-		}
+			printf("is negative");
 	return (0);
 }

6a95f5fdead5299612e9b308ad79f8d32a2b65fc
bigBadMatt <5707@holbertonstudents.com>
2022-10-02 18:16:45 -0700

    program to print all the numbers between 0 and 9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - function to print all numbers between 0 and 9
+ *Return: zero at all times
+ */
+
+int main(void)
+{
+	int ch = 48;
+
+	for (; ch <= 57; ch++)
+	{
+		putchar(ch);
+		putchar(39);
+		putchar(32)
+	}
+	putchar('\n');
+	return (0);
+}

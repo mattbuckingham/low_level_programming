0ae443deb8526900f37d1038bce87d7223c32b3a
bigBadMatt <5707@holbertonstudents.com>
2022-10-03 18:10:46 -0700

    fixed typo in function call

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,16 +1,14 @@
 #include "main.h"
+
 /**
- *Main - 
- *Return:
+ *print_alphabet - fuinction that prints the alphabet using _putchar
  */
 
-void print_alphabet (void)
+void print_alphabet(void)
 {
 	char ch = 'a';
 
 	for (; ch <= 'z'; ch++)
-		putchar(ch);
-	putchar('\n');
-	return;
+		_putchar(ch);
+	_putchar('\n');
 }
-

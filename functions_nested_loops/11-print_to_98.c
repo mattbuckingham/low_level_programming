51221499acabfe4417652bea1728f0e14dbc31cb
bigBadMatt <5707@holbertonstudents.com>
2022-10-08 14:48:08 -0700

    function to print all numbers from n to 98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * print_to_98 - function to print all numbers from n to 98
+ * @n: a number to print to 98 from
+ * Return: void
+ */
+
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n < 98)
+			{
+				printf(", ");
+			}
+			n = n - 1;
+		}
+	}
+
+	else
+	{
+		while (n <= 98)
+		{
+			printf("%d", n);
+			if (n < 98)
+			{
+				printf(", ");
+			}
+			n = n + 1;
+
+	}
+
+}

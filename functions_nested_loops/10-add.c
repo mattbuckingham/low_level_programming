6cab7c6b16de9c6857b8d44317140f3e39791a04
bigBadMatt <5707@holbertonstudents.com>
2022-10-08 14:30:26 -0700

    updated comments

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,7 +1,9 @@
 #include "main.h"
 /**
- *main -prints '_putchar' to output
- *Return: zero at all times
+ *add - returns the sum of two numbers
+ *Return: the sum of two numbers
+ * @num1: number to be added
+ * @num2: number to be added
  */
 
 int add(int num1, int num2)

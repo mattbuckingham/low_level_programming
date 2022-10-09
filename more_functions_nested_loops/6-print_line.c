ab1628eb8cb0d6d0417952c8c6586b18db08536e
bigBadMatt <5707@holbertonstudents.com>
2022-10-08 22:03:47 -0700

    function that prints a line of length n

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_line - prints a line the length of n
+ * Return: void
+ * @n : the length of line to be printed
+ */
+
+void print_line(int n)
+{
+	while (n < 0)
+	{
+		_putchar('_');
+		n = n - 1;
+	}
+	_putchar('\n')
+}

befa795b5b84da262ac58a64826ccac0ff06a894
bigBadMatt <5707@holbertonstudents.com>
2022-10-09 22:47:37 -0700

    fixed using string like an int for while loop, now uses iteration var

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -8,21 +8,22 @@ void print_rev(char *s)
 	int i;
 
 	i = 0;
-	/** this loop finds the length of the string, which is now c*/
+	/** this loop finds the length of the string, which is where the pointer will now point*/
 	while (*s != '\0')
 	{
 		s = s + 1;
-		i = i + 1;
 	}
 
 	/** increment back off the null char */
 	s = s - 1;
 
 	/* print the string from last to first */
-	while (s > 0)
+	i = s;
+	while (i > 0)
 	{
 		_putchar(*s);
 		s = s - 1;
+		i = i - 1;
 	}
 
 

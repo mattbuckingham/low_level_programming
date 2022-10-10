80cc378873ff39319a98709b1b4d776bd4595176
bigBadMatt <5707@holbertonstudents.com>
2022-10-09 22:31:37 -0700

    function to print a string backwards

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+ #include "main.h"
+/**
+ * print_rev - prints a string to output
+ * @s: a string to be printed backwards
+ */
+void print_rev(char *s)
+{
+	int c;
+	int *i;
+
+	i = s;
+	/** this loop finds the length of the string, which is now c*/
+	while (*i != '\0')
+	{
+		c = c + 1;
+		i = i + 1;
+	}
+
+	while (c > 0)
+	{
+		_putchar(*(s+c));
+		c = c - 1;
+	}
+
+
+	_putchar('\n');
+}

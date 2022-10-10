c85be29978d164d98fa3e48eb4909062912e5181
bigBadMatt <5707@holbertonstudents.com>
2022-10-09 22:41:15 -0700

    renmoved unneeded iteration var, now iterates off the pointer value

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -5,21 +5,24 @@
  */
 void print_rev(char *s)
 {
-	int c;
-	char *i;
+	int i;
 
-	i = s;
+	c = 0;
 	/** this loop finds the length of the string, which is now c*/
-	while (*i != '\0')
+	while (*s != '\0')
 	{
-		c = c + 1;
+		s = s + 1;
 		i = i + 1;
 	}
 
+	/** increment back off the null char */
+	s = s - 1;
+
+	/* print the string from last to first */
 	while (c > 0)
 	{
-		_putchar(*(s + c));
-		c = c - 1;
+		_putchar(*s);
+		s = s - 1;
 	}
 
 

f431a5f3943fd1d5780a708c09ebadf794bf4878
bigBadMatt <5707@holbertonstudents.com>
2022-10-13 04:05:02 -0700

    a function to print the first n elements of an array

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_array - prints the first n elements of an array
+ * @a: an array to be printed
+ * @n an int of how many elements to be printed
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+		printf("%d, " a[i]);
+		i = i + 1;
+	}
+	_putchar('\n');
+}

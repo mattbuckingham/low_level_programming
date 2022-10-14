96dc531303f410b3e5f9c4f8978889099b536fdb
bigBadMatt <5707@holbertonstudents.com>
2022-10-13 17:19:20 -0700

    added missing comparative operator

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *_strncpy - copy the first N elements of one string to another
+ *_strcmp - copy the first N elements of one string to another
  * @s1: string to be compared
  * @s2: string to be compared
  * Return: the result of s1 - s2
@@ -14,11 +14,11 @@ int _strcmp(char *s1, char *s2)
 	i = 0;
 	res = 0;
 
-	/*iterare until the element prior to the first nullchar of either string*/
-	while ( s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
+/*iterare until an unequal element prior to the first nullchar of either string*/
+	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
 	{
 		i = i + 1;
 	}
-
+	res = s1[i] - s2[i];
 	return (res);
 }

926c64c715be2b1f7e08d48480a07e230192777c
bigBadMatt <5707@holbertonstudents.com>
2022-10-14 15:41:49 -0700

    funtion to copy from one array to another

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _memset - copies n bytes from memory area src to memory area dest
+ * @dest: the location to copy to
+ * @src: the location to be copied
+ * @n: number of bytes to be copied
+ * Return: the destination array
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i = i + 1;
+	}
+	return (dest);
+}

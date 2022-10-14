7f170b1528cb023628a52843904c906e195550d3
bigBadMatt <5707@holbertonstudents.com>
2022-10-14 15:04:58 -0700

    memset function

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/*
+ *_memset - The _memset() function fills the first n bytes of the memory area 
+ * pointed to by s with the constant byte b
+ * @s: target array
+ * @b: byte to be written
+ * @n: number of bytes to fill
+ * Return: the array
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+		s[i] = b;
+		i = i + 1;
+	}
+	return (s);
+}

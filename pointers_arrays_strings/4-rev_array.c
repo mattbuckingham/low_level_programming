733b81eaea03f5492b413816800a5e7533617f7e
bigBadMatt <5707@holbertonstudents.com>
2022-10-13 17:58:57 -0700

    adjusted var assignment to account for arrays starting at 0

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -12,11 +12,11 @@ void reverse_array(int *a, int n)
 	int tmp;
 
 	i = 0;
-	while (i < (n / 2))
+	while (i < (((n / 2)-1))
 	{
 		tmp = a[i];
-		a[i] = a[(n-i)];
-		a[(n-i)] = tmp;
+		a[i] = a[((n - i)-1)];
+		a[(n - i)] = tmp;
 		i = i + 1;
 	}
 }

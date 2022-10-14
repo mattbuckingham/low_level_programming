7c727a982dce035f256b0a7fa14b184092bef409
bigBadMatt <5707@holbertonstudents.com>
2022-10-13 21:48:16 -0700

    function that capitalises the start of words

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @arr: a string
+ * Return: void
+ */
+
+char *string_toupper(char *arr)
+{
+	int i;
+	int inc;
+
+	i = 0;
+/*work out the size of the difference in ascii value between lower and upper */
+	inc = 'A' - 'a';
+
+	while (arr[i] != '\0')
+	{
+/*this beast of a statement checks to see if the previous char was non-alpha*/
+		if (arr[i] >= 'a' && arr[i] <= 'z' && arr[(i - 1)] < 'a' &&
+		    arr[(i - 1)] > 'z' && arr[(i - 1)] < 'A' && arr[(i - 1)] &&
+		    arr[(i - 1)] > 'Z')
+		{
+			arr[i] = arr[i] + inc;
+		}
+		i = i + 1;
+	}
+	return (arr);
+}

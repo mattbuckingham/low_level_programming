da60944f92329110240df06291afa964af541ce6
bigBadMatt <5707@holbertonstudents.com>
2022-10-13 22:51:47 -0700

    re-formatted to make conditional statement much easier to understand

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -17,11 +17,20 @@ char *cap_string(char *arr)
 	while (arr[i] != '\0')
 	{
 /*this beast of a statement checks to see if the previous char was non-alpha*/
-		if (arr[i] >= 'a' && arr[i] <= 'z' && (arr[(i - 1)] < 'a' ||
-		    arr[(i - 1)] > 'z' && arr[(i - 1)] < 'A' && arr[(i - 1)] ||
-			arr[(i - 1)] > 'Z'))
+		if ((arr[i] >= 'a' && arr[i] <= 'z')
 		{
-			arr[i] = arr[i] + inc;
+			if ((arr[(i - 1)] < 'a')
+			{
+				arr[i] = arr[i] + inc;
+			}
+			else( arr[(i - 1)] > 'z' && arr[(i - 1)] < 'A')
+			{
+				arr[i] = arr[i] + inc;
+			}
+			else ( arr[(i - 1)] > 'Z'))
+			{
+				arr[i] = arr[i] + inc;
+			}
 		}
 		i = i + 1;
 	}

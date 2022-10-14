6c305d0dae291ac8c2ee2c944413178fdc197cbc
bigBadMatt <5707@holbertonstudents.com>
2022-10-13 19:58:14 -0700

    function to convert lowercase chars of a strung to uppercase

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @arr: a string
+ * Return: void
+ */
+
+char *string_toupper(char *arr)
+{
+	int i;
+	int inc;
+
+/*work out the size of the difference in ascii value between lower and upper */
+	inc = 'A' - 'a';
+
+	while (arr[i] != '\0')
+	{
+		if (arr[i] > 'a' && arr[i] < 'z')
+		{
+			arr[i] = arr + inc;
+			i = i + 1;
+		}
+	}
+}

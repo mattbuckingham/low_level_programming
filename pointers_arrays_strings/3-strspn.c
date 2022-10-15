8db02b5013c64f7f970e9efff4bb927ed93b7e45
bigBadMatt <5707@holbertonstudents.com>
2022-10-14 20:33:25 -0700

    function that returnrs the number of bytes in a prefix substring

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _strspn - function that gets the length of a prefix substring.
+ * @s: target array
+ * @accept: the target substring
+ * Return: unsigned int
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, k;
+	unsigned int n;
+
+	i = 0;
+	k = 0;
+	n = 0;
+
+	while (s[i] != '\0')
+	{
+		k = 0;
+		while (accept[k] != '\0')
+		{
+			if (s[i] == accept[k])
+			{
+				n = n + 1;
+			}
+			k = k + 1;
+		}
+		i = i + 1;
+	}
+
+	return (n);
+}

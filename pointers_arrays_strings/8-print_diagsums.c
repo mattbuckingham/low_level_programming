b8650189a2ec770b2755357727cb839e2b7b7d68
bigBadMatt <5707@holbertonstudents.com>
2022-10-15 15:29:35 -0700

    function to print the sums of two diagonals of a matrix

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * 
+ * @
+ * @
+ * Return:
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, k;
+	int t1, t2;
+
+	i = 0;
+	k = 0;
+	t1 = 0;
+	t2 = 0;
+
+	/* calculate the first diagonal, stored in t1*/
+	while (i < size)
+	{
+		t1 = t1 + a[(i * size) + k];
+		k = k + 1;
+		i = i + 1;
+	}
+	/*set k = size to move in the opposite direction as we move down*/
+	i = 0;
+	k = size;
+	/*calculate the second diagonal, stored in t2*/
+	while (i < size)
+	{
+		t2 = t2 + a[(i * size) + k];
+		i = i + 1;
+		k = k - 1;
+	}
+
+	/*print our results*/
+	printf("%d, %d\n", t1, t2)
+}

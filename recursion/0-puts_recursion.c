448244a4f2520c5fbb803599b753ccfc75cfa2c6
bigBadMatt <5707@holbertonstudents.com>
2022-10-17 16:22:11 -0700

    changed documentation to suit linter

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _puts_recursion - function that prints a string, followed by a new line
- * @s:
+ * @s: a string to be printed
  * Return:
  */
 
@@ -10,7 +10,7 @@ void _puts_recursion(char *s)
 	if (*s != '\0')
 	{
 		_putchar(*s);
-		_puts_recursion(s+1);
+		_puts_recursion(s + 1);
 	}
 	else
 	{

4434a42593396e87dd9d3d66407e93aec0fa7722
bigBadMatt <5707@holbertonstudents.com>
2022-10-18 17:56:40 -0700

    added increment to the pointer address in recusive calls

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -10,11 +10,11 @@ void _print_rev_recursion(char *s)
 
 	if (*s == '\0')
 	{
-		return;
+		return ();
 	}
 	else
 	{
-		_print_rev_recursion(s);
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
 }

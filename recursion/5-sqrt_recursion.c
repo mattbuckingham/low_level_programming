47ee6efb0612519414cb99bc1692a5411af681d3
bigBadMatt <5707@holbertonstudents.com>
2022-10-20 15:41:31 -0700

    function to find the square root of a number

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _sqrt_rec - returns the natural square root of a number
+ * @n: a number with a square root
+ * @j: the square root of n
+ * Return: the square root of n
+ */
+int _sqrt_rec(int n, int r)
+{
+	/*edge case for n being zero or negaive*/
+	if (n < 1)
+	{
+		return (-1);
+	}
+/*break case for successfully finding the square root*/
+	if (r * r == n)
+	{
+		return (r);
+	}
+	/*break case for no result */
+	if (n == r)
+	{
+		return (-1);
+	}
+	return (_sqrt_rec(n, r + 1));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n:
+ * @:
+ * Return: the square root of n
+ */
+int _sqrt_recursion(int n)
+{
+	return (_sqrt_rec(n, 2));
+}

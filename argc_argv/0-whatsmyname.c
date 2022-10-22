7bb17028117ae56f26e3bb1fc1618d7a70b074ce
bigBadMatt <5707@holbertonstudents.com>
2022-10-21 23:24:16 -0700

    fixed formatting to satisfy linter

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -4,14 +4,14 @@
  *main - print the name of the executable that this program is run from
  * @argc:argc
  * @argv: argv
- * Return: zero 
+ * Return: zero
  */
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	if ( argc == 1)
+	if (argc == 1)
 	{
-		argc = 1;;
+		argc = 1;
 	}
 	printf("%s\n", argv[0]);
 	return (0);

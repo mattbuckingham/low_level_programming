f1adeff5e08c48fb0ff78a980d0db04d760d69a6
bigBadMatt <5707@holbertonstudents.com>
2022-10-21 23:15:05 -0700

    program that prints the executable name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+/**
+ *main - print the name of the executable that this program is run from
+ * @argc:argc
+ * @argv: argv
+ * Return: zero 
+ */
+
+int main (int argc, int *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}

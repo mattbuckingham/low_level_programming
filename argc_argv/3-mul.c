b4d0af39cbd5b31d9588b14b3bb5c2fa4c52e7ad
bigBadMatt <5707@holbertonstudents.com>
2022-10-22 15:30:43 -0700

    fixed print statement expecting a char and receiving an int

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -10,10 +10,11 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc == 1)
+	if (argc != 3)
 	{
-		argc = 1;
+		printf("Error\n");
+		return (1);
 	}
-	printf("%s\n", atoi(argv[1]) * atoi(argv[2]));
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }

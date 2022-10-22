2b5d4c86bf695396ea5d8ea7de151c4c5fa1a402
bigBadMatt <5707@holbertonstudents.com>
2022-10-22 15:35:54 -0700

    program that prints all arguments passed to it

---
+++ argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - print arguments passed to it
+ * @argc:argc
+ * @argv: argv
+ * Return: zero
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	i = 0;
+	while(i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i = i + 1;
+	}
+	return (0);
+}

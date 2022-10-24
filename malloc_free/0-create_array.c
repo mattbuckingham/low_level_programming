40d951e51a0e4a72419abc93d9650b9572936699
bigBadMatt <5707@holbertonstudents.com>
2022-10-24 13:52:37 -0700

    function that creates an array of chars

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -3,13 +3,35 @@
 #include <mem.h>
 
 /**
- *
- *
- *
- *
+ * create_array - creates an array of chars, initializes it with a specific char
+ * @size: the size of array to be made
+ * @c: a char to fill the array with
+ * Return: the arrary
  */
 
 char *create_array(unsigned int size, char c)
 {
-	
+	int i;
+	char *arr;
+
+	/*error check, I think I can just check the value that malloc(0) returnns tho*/
+	if (size <= 0)s
+	{
+		return (NULL);
+	}
+
+	arr = malloc(size * 1);
+
+	if (arr == 0)
+	{
+		return (NULL);
+	}
+
+	while (i < size)
+	{
+		arr[i] = c;
+		i = i + 1;
+	}
+	return (arr);
+
 }
---
+++ malloc_free/README.md
@@ -0,0 +1,2 @@
+<h1>C - malloc, free</h1>
+<h2>By Matthew Buckingham</h2>

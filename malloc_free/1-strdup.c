597cd284191fc8bba4c1e68a7c985322ac5ac6a8
bigBadMatt <5707@holbertonstudents.com>
2022-10-24 14:49:17 -0700

    a function that duplicates a string and returns a pointer to its address

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -2,8 +2,54 @@
 #include <stdlib.h>
 
 /**
- *
- *
- *
- *
+ * _strlen - a function to return the length of a string
+ * @s: a string
+ * Return: the length of a string
  */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*s != '\0')
+	{
+		i = i + 1;
+		s = s + 1;
+	}
+	return (i);
+}
+
+/**
+ * strdup - duplicates a string
+ * @str: a string to be duplicated
+ * Return: a pointer to the duplicated string
+ */
+char *_strdup(char *str)
+{
+	int i;
+	int len;
+	char *str_dup;
+
+	i = 0;
+/*I'm not sure that this works*/
+	if (*str == '\0')
+	{
+		return (NULL);
+	}
+
+	len = _strlen(str);
+	str_dup = malloc(len * 1);
+
+/*check is malloc call was successful */
+	if (str_dup == 0)
+	{
+		return (NULL);
+	}
+	/*assign each element of the duplicate string to that of the origional*/
+	while (i < len)
+	{
+		str_dup[i] = str[i];
+	}
+	return (str_dup);
+}

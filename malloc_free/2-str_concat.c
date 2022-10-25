94423dc5df6bd69155fca99c98992fe4cb1213a8
bigBadMatt <5707@holbertonstudents.com>
2022-10-24 20:04:18 -0700

    function that concatenates two strings and returns a pointer to the result

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strlen - a function to return the length of a string
+ * @s: a string
+ * Return: the length of a string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*s != '\0')
+	{
+		i = i + 1;
+		s = s + 1;
+	}
+	return (i);
+}
+
+/**
+ *
+ *
+ *
+ *
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *str_cat;
+	int len;
+	int i;
+
+	/*we set this to 1 to allow space for the nullchar*/
+	len = 1;
+	if (s1 != NULL)
+	{
+		len = len + _strlen(s1);
+	}
+	if (s2 != NULL)
+	{
+		len = len + _strlen(s2);
+	}
+
+	/*allocate an adequate amount om memory*/
+	str_cat = (char *)malloc(sizeof(str_cat) * len);
+
+	i = 0;
+	putchar(len);
+
+	while (i < (len - 1))
+	{
+		if (i < _strlen(s1))
+		{
+			str_cat[i] = (s1[i]);
+		}
+		else
+		{
+			str_cat[i] = s2[(i - _strlen(s1))];
+		}
+		i = i + 1;
+		printf ("%c\n", str_cat[i]);
+	}
+	/*insert nullchar to terminate string*/
+	str_cat[len - 1] = '\0';
+	return (str_cat);
+}

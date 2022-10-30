05e74a6b9e42f6ede70df3b6770dd4ee84f84268
bigBadMatt <5707@holbertonstudents.com>
2022-10-30 02:24:14 -0700

    fixed formatting to satisfy linter

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -3,18 +3,19 @@
 
 /**
  * _calloc - function that allocates memory for an array, using malloc
- *@nmemb:
- *@size:
- *Return:
+ *@nmemb: the size of the parrent array
+ *@size: the size opf the sub arrays
+ *Return: a pointer to the array
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *arr;
+	char *arr;
 	unsigned int i;
 
 	if (size == 0 || nmemb == 0)
 	{
 		return (NULL);
+
 	}
 
 	arr = malloc(nmemb * size);

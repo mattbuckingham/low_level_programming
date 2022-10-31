42a4dbe1e6773b03c288a435793bf41095e34879
bigBadMatt <5707@holbertonstudents.com>
2022-10-31 14:55:21 -0700

    fixed formatting to satisfy linter

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,12 +1,12 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
 /**
  * struct dog - structure
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
  */
-
-#ifndef _DOG_H_
-#define _DOG_H_
 struct dog
 {
 	char *name;

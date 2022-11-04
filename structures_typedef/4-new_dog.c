00500aed535b36ec794b1dca965fd39f9e0d56b0
bigBadMatt <5707@holbertonstudents.com>
2022-11-03 18:12:56 -0700

    fixed implementation of previous change

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -32,7 +32,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t doggo;
 	int i;
 
-	doggo = malloc(sizeof(*dog_t));
+	doggo = malloc(sizeof(*doggo));
 	if (doggo == NULL)
 	{
 		return (NULL);

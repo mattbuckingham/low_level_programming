2d6922d746af8cdaba2ab4bfb8b02b0ec34f296e
bigBadMatt <5707@holbertonstudents.com>
2022-11-04 18:18:57 -0700

    funtion that frees dog, all dogs should be free :)

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ *free_dog - function that frees a dog
+ * @d: a dog to be freed
+ *
+*/
+
+void free_dog(dog_t *d)
+{
+	free(d->owner);
+	free(d->name);
+	free(d);
+	return;
+}

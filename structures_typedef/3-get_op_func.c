ff254e5c09bf2037ea2dc6c0972aed7afeeaa40b
bigBadMatt <5707@holbertonstudents.com>
2022-11-05 16:22:37 -0700

    program that performs basic artithmatic on user input

---
+++ structures_typedef/3-get_op_func.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "calc.h"
+
+/**
+ * get_op_func - elects the correct function to perform the operation requested
+ * @s: operator passed as argument to the program
+ * Return: the result
+*/
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+	while (i < 6)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+		{
+			return (ops[i].f);
+		}
+		i = i + 1;
+	}
+	return (0);
+}
---
+++ structures_typedef/3-op_functions.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * op_add - returns the sum of two numbers
+ * @a: number
+ * @b: number
+ * Return: the result of a + b
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - returns the difference of two numbers
+ * @a: number
+ * @b: number
+ * Return: the result of a - b
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - returns the product of two numers
+ * @a: number
+ * @b: number
+ * Return: the result of a * b
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - returns the quotient of two numbers
+ * @a: number
+ * @b: number
+ * Return: the result of a / b
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - returns the modulus of two numbers
+ * @a: number
+ * @b: number
+ * Return - the result of a % b
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}

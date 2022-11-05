031215ab7f5b4c9f6cd57cb8fafce9d50efbd038
bigBadMatt <5707@holbertonstudents.com>
2022-11-05 16:28:46 -0700

    program that performs basic artithmatic on user input

---
+++ function_pointers/3-main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - main function
+ * @argc: number of agruments
+ * @argv : an array containing the arguments
+ * Return: 0 at all times
+*/
+int main(int argc, char *argv[])
+{
+	int n1, n2;
+	char *op;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[3]);
+	op = argv[2];
+
+	if (get_op_function(op) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((op == '/' || op == '%') && n2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", get_op_func(op)(n1, n2));
+
+	return (0);
+}

305bd21c63875a62031af45ec0de33f70a2311c9
bigBadMatt <5707@holbertonstudents.com>
2022-11-04 22:28:13 -0700

    fixed checking pointer address for null

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -11,7 +11,7 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
-	if ( array != NULL && size != NULL && action != NULL)
+	if ( array != NULL && action != NULL)
 	{
 		while (i < size)
 		{

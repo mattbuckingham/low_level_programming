a6f6f37dcc81aec836302efc9e116b0864096f24
bigBadMatt <5707@holbertonstudents.com>
2022-11-04 22:25:10 -0700

    added check for null values, fixed comparing unsigned int with int

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "function_pointers.h"
 
 /**
- *
- *
- *
+ * array_iterator -executes a function given as a parameter on an array.
+ * @array: an array to perform a function on
+ * @size: the size of the array
+ * @action: a function to execute
 */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+	if ( array != NULL && size != NULL && action != NULL)
+	{
+		while (i < size)
+		{
+			action(array[i]);
+			i = i + 1;
+		}
+	}
+}

533018e428a34612399087c5010a493dc7beb367
bigBadMatt <5707@holbertonstudents.com>
2022-11-05 23:04:40 -0700

    fixed comparing int to pointer

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -16,7 +16,7 @@ void print_all(const char * const format, ...)
 
 	va_start(all, format);
 
-	while (format != NULL && format[i] != NULL )
+	while (format != NULL && format[i])
 	{
 		str = ", ";
 		if (format[i] == 'c')

41285bd165e5591eea6bbab7fc7b0d2425674c44
bigBadMatt <5707@holbertonstudents.com>
2022-11-06 02:47:25 -0800

    removed commented out code

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -89,37 +89,6 @@ void print_all(const char * const format, ...)
 		}
 		i = i + 1;
 	}
-/*
-	int i;
-	char *str;
-	char plc; 
-	while (format != NULL && format[i])
-	{
-		str = ", ";
-		if (format[i] == 'c')
-		{
-			printf("%c%s", va_arg(all, int), str);
-		}
-		else if (format[i] == 'i')
-		{
-			printf("%d%s", va_arg(all, int), str);
-		}
-		else if (format[i] == 'f')
-		{
-			printf("%f%s", va_arg(all, double), str);
-		}
-		else if (format[i] == 's')
-		{
-			plc = *va_arg(all, char *);
-
-			if (plc == NULL)
-			{
-				plc = "(nil)";
-			}
-			printf("%s%s", plc, str);
-		}
-		i = i + 1;
-	}*/
 
 	printf("\n");
 	va_end(all);

52ef2b34658894f8ba641db34c3c2ce52f723d40
bigBadMatt <5707@holbertonstudents.com>
2022-11-05 18:33:53 -0700

    fixed typo in var name

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -29,7 +29,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("(nil)");
 		}
 
-		if (i < (n - 1) && seperator != NULL)
+		if (i < (n - 1) && separator != NULL)
 		{
 			printf("%s", separator);
 		}

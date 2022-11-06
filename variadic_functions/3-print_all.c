0ae8e3657b31d835e38d78d0b7a2b47de303de67
bigBadMatt <5707@holbertonstudents.com>
2022-11-06 02:45:56 -0800

    added updated header file, refactored to use function pointer in place of giant if/else statement

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -3,6 +3,57 @@
 #include "variadic_functions.h"
 
 /**
+ * print_char - print a char
+ * @n:char to be printed
+ * Return: 0 at all times
+ */
+int print_char(va_list all)
+{
+	printf("%c", va_arg(all, int));
+	return 0;
+}
+
+/**
+ * print_integer - print an int
+ * @n: int to be printed
+ * Return: 0 at all times
+ */
+int print_integer(va_list all)
+{
+	printf("%d", va_arg(all, int));
+	return 0;
+}
+
+/**
+ * print_float - print a float
+ * @n: float to be printed
+ * Return: 0 at all times
+ */
+int print_float(va_list all)
+{
+	printf("%f", va_arg(all, double));
+	return 0;
+}
+
+/**
+ * print_str - print a string
+ * @n:string to be printed
+ * Return: 0 at all times
+ */
+int print_string(va_list all)
+{
+	char *plc;
+	plc = va_arg(all, char *);
+
+	if (plc == NULL)
+	{
+		plc = "(nil)";
+	}
+	printf("%s", plc);
+	return 0;
+}
+
+/**
  * print_all - print all things
  * @format: list of all types to be printed
  *
@@ -10,12 +61,38 @@
 void print_all(const char * const format, ...)
 {
 	va_list all;
+	int i, k;
+	sel pri[] = {
+		{"c", print_char},
+		{"i", print_integer},
+		{"f", print_float},
+		{"s", print_string},
+		{NULL, NULL}
+	};
+	va_start(all, format);
+	i = 0;
+
+	while (format != NULL && format[i])
+	{
+		k = 0;
+		while (pri[k].f != NULL)
+		{
+			if (format[i] == *pri[k].c)
+			{
+				pri[k].f(all);
+			}
+		        k = k + 1;
+		}
+		if (i != 0)
+		{
+			printf(", ");
+		}
+		i = i + 1;
+	}
+/*
 	int i;
 	char *str;
 	char plc; 
-
-	va_start(all, format);
-
 	while (format != NULL && format[i])
 	{
 		str = ", ";
@@ -42,7 +119,8 @@ void print_all(const char * const format, ...)
 			printf("%s%s", plc, str);
 		}
 		i = i + 1;
-	}
+	}*/
+
 	printf("\n");
 	va_end(all);
 }
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,5 +1,17 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
+#include <stdarg.h>
+/**
+ * struct  - 3-main.c
+ * @c: character to check
+ * @f: function to point to
+ */
+typedef struct op
+{
+	char *c;
+	int (*f)(va_list a);
+} sel;
+
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);

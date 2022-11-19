79d50b23c3942ce94b68066282a470075772d4fb
bigBadMatt <5707@holbertonstudents.com>
2022-11-18 20:49:25 -0800

    added edgecase for head already being null

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -25,6 +25,9 @@ void free_listint(listint_t *head)
 */
 void free_listint2(listint_t **head)
 {
-	free_listint(*head);
-	*head = NULL;
+	if (head != NULL)
+	{
+		free_listint(*head);
+		*head = NULL;
+	}
 }

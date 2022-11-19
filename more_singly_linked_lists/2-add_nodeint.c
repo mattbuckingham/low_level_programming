cef9c2b646fc80cce822181b38a7f0bf6fefc15a
bigBadMatt <5707@holbertonstudents.com>
2022-11-18 17:43:07 -0800

    fixed assigning a value when var type was pointer

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -13,14 +13,14 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	listint_t *new_node;
 
 	new_node = malloc(sizeof(listint_t));
-	if (new == NULL)
+	if (new_node == NULL)
 	{
 		return (NULL);
 	}
 	new_node->n = (int)n;
-	new_node->next = head;
+	new_node->next = *head;
 
 	*head = new_node;
 
-	return (head);
+	return (*head);
 }

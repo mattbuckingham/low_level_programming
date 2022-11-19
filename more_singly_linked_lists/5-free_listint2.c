0eae19e4d5c989c235b29f61299cfc414755e7ba
bigBadMatt <5707@holbertonstudents.com>
2022-11-18 20:47:25 -0800

    function tha tfrees a list

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint - function that frees a listint_t list.
+ * @head: a pointer to the head of a linked list to be freed
+*/
+void free_listint(listint_t *head)
+{
+	if (head != NULL)
+	{
+		free_listint(head->next);
+		free(head);
+	}
+	else
+	{
+		return;
+	}
+}
+
+/**
+ * free_listint2 - function that frees a listint_t list.
+ * @head: a pointer to the head of a linked list to be freed
+*/
+void free_listint2(listint_t **head)
+{
+	free_listint(*head);
+	*head = NULL;
+}

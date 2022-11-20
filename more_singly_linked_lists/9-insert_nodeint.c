6855d83d4a5cee39bd35224c23ad99dd225378fa
bigBadMatt <5707@holbertonstudents.com>
2022-11-20 02:54:10 -0800

    fixed formatting to satisfy linter

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -28,7 +28,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new_node;
 		return (new_node);
 	}
-	else while (i < idx && (*head)->next != NULL)
+	while (i < idx && (*head)->next != NULL)
 	{
 		*head = (*head)->next;
 		i = i + 1;

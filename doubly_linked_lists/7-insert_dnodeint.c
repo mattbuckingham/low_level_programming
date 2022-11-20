842bad708ab619b4de998020f97c0352a3bd7c21
bigBadMatt <5707@holbertonstudents.com>
2022-11-20 04:13:50 -0800

    added edgecase check for index being outside of list

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -32,6 +32,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		tmp = tmp->next;
 	}
 
+	if (tmp == NULL && i < idx)
+	{
+		return (NULL);
+	}
+
 	new_node->n = n;
 	new_node->prev = tmp;
 	new_node->next = tmp->next;

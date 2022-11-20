9ba6f1f382eee144b87feb31189cce99e5f11375
bigBadMatt <5707@holbertonstudents.com>
2022-11-20 03:10:38 -0800

    made change

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -34,7 +34,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		i = i + 1;
 	}
 
-	if (*head->next == NULL && i < idx)
+	if (*head == NULL)
 	{
 		return (NULL);
 	}

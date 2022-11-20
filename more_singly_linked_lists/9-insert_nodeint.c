e2179ce5f71902d05edca82fd16683afd537d28d
bigBadMatt <5707@holbertonstudents.com>
2022-11-20 02:05:37 -0800

    fixed var that was meant to be a pointer but was declared as a listint_t instead

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -12,7 +12,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;
-	listint_t new_node;
+	listint_t *new_node;
 
 	i = 0;
 	new_node = malloc(sizeof(listint_t));

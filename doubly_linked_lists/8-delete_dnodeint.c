deaffe15c599ac8b636e74ecc66abbd39d382af4
bigBadMatt <5707@holbertonstudents.com>
2022-11-20 04:51:23 -0800

    fixed formatting to satisfy linter

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -28,7 +28,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		}
 		*head = tmp->next;
 		free(tmp);
-		return(1);
+		return (1);
 	}
 	while (i < index && tmp != NULL)
 	{
@@ -48,5 +48,6 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		tmp->next->prev = tmp->prev;
 		tmp->prev->next = tmp->next;
 	}
+	free(tmp);
 	return (1);
 }

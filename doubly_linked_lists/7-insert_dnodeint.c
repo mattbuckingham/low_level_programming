f100cfa26d2e3277a2d88715f573097b7ab871ef
bigBadMatt <5707@holbertonstudents.com>
2022-11-20 04:11:38 -0800

    added missing = operator in comparison statement

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -17,7 +17,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	tmp = *h;
 	i = 0;
 	new_node = malloc(sizeof(dlistint_t));
-	if (new_node = NULL)
+	if (new_node == NULL)
 	{
 		return (NULL);
 	}
@@ -35,7 +35,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->n = n;
 	new_node->prev = tmp;
 	new_node->next = tmp->next;
-	tmp->next->prev = new;
+	tmp->next->prev = new_node;
 	tmp->next = new_node;
 
 	return (new_node);

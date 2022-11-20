cfa07663dee182f5a786f3b14d40eb8d183ccef3
bigBadMatt <5707@holbertonstudents.com>
2022-11-20 02:17:30 -0800

    created temp var so that function didnt need to move the head pointer

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -12,9 +12,10 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;
-	listint_t *new_node;
+	listint_t *new_node, *tmp;
 
 	i = 0;
+	*tmp = *head;
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 	{
@@ -24,24 +25,25 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node->n = n;
 	if (idx == 0)
 	{
-		new_node->next = *head;
-		*head = new_node;
+		new_node->next = tmp;
+		tmp = new_node;
 		return (new_node);
 	}
-	else while (i < idx && (*head)->next != NULL)
+	
+	while (i < (idx - 1) && tmp->next != NULL)
 	{
-		*head = (*head)->next;
+		tmp = tmp->next;
 		i = i + 1;
 	}
 
-	if ((*head)->next == NULL && i < idx)
+	if (tmp->next == NULL && i < idx)
 	{
 		return (NULL);
 	}
 	else
 	{
-		new_node->next = *head;
-		(*head)->next = new_node;
+		new_node->next = tmp;
+		tmp->next = new_node;
 		return (new_node);
 	}
 }

11807e079dbdf747cfa4cc40fd8e05b629e1f3dd
bigBadMatt <5707@holbertonstudents.com>
2022-11-20 03:09:08 -0800

    made change

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -28,13 +28,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new_node;
 		return (new_node);
 	}
-	while (i < idx && (*head)->next != NULL)
+	while (i < idx && *head != NULL)
 	{
 		*head = (*head)->next;
 		i = i + 1;
 	}
 
-	if ((*head)->next == NULL && i < idx)
+	if (*head->next == NULL && i < idx)
 	{
 		return (NULL);
 	}

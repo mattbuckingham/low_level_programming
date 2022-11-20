d16d4987b8f96ade87b6a798055b627ddfded0d1
bigBadMatt <5707@holbertonstudents.com>
2022-11-20 13:42:53 -0800

    removed offset to index comarison, a relic from when this code was dealing with doubly linked lists

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -26,7 +26,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		free(tmp);
 		return (1);
 	}
-	while (i < index && tmp-> != NULL)
+	while (i < index && tmp->next != NULL)
 	{
 		node = tmp;
 		tmp = tmp->next;

59067d9c47ec149640c98d6e8b94bbaa08adcab5
bigBadMatt <5707@holbertonstudents.com>
2022-11-20 03:50:50 -0800

    added edgecase check for index being outside of list

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -13,8 +13,13 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	unsigned int i;
 
 	i = 0;
+
 	while (i < index)
 	{
+		if (head == NULL)
+		{
+			return (NULL);
+		}
 		head = head->next;
 		i = i + 1;
 	}

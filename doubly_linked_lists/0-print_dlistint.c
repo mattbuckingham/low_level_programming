c2e39bce3a56626869c9f097808cbd1b72c80d58
bigBadMatt <5707@holbertonstudents.com>
2022-11-20 03:07:29 -0800

    changed condition statement

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -5,14 +5,14 @@
 /**
  * print_dlistint - function that prints all the elements of a dlistint_t list.
  * @h: pointer to the header of the list
- * return: count of nodes.
+ * Return: count of nodes.
 */
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count;
 
 	count = 0;
-	while (h->next != NULL)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;

113720dcd0282b4e410926b89fe45c39c77af048
bigBadMatt <5707@holbertonstudents.com>
2022-11-20 02:23:25 -0800

    changed var to assign head value, not head address

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node, *tmp;
 
 	i = 0;
-	*tmp = head;
+	tmp = *head;
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 	{

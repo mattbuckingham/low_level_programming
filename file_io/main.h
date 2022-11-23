73713a69a7f40a666a44bfcb2339fd2f492b2ab3
bigBadMatt <5707@holbertonstudents.com>
2022-11-22 19:36:37 -0800

    updated header to include include statements

--- file_io/main.h
+++ file_io/main.h
@@ -1,4 +1,16 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+#include <stdio.h>
+#include <stdlid.h>
+#include <sys/types>
+#include <sys/stat.h>
+#include <fnctl.h>
+#include <unistd.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+
 #endif /* _MAIN_H_ */

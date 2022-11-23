de5faf6ba70e24ac4d5bbc42cfe44a04d77ce984
bigBadMatt <5707@holbertonstudents.com>
2022-11-22 20:51:32 -0800

    added statement to set permissions

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -19,15 +19,19 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 	{
 		fd = open(filename, O_CREAT | O_WRONLY);
+		if (fd == -1)
+		{
+			return (-1);
+		}
+		return (1);
 	}
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 	{
-		close(fd);
 		return (-1);
 	}
-	wr = write (fd, text_content, strlen(text_content));
+	wr = write(fd, text_content, strlen(text_content));
 	if (wr == -1)
 	{
 		close (fd);

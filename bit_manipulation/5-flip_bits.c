4c299c7182a6caa04e1e29739dea48ac85b8c1b5
bigBadMatt <5707@holbertonstudents.com>
2022-11-24 12:57:38 -0800

    added missing initialisation statement

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -3,7 +3,7 @@
 #include "main.h"
 
 /**
- * flip_bits - function that returns the number of bits you would need to flip 
+ * flip_bits - function that returns the number of bits you would need to flip
  * to get from one number to another.
  * @n: a starting number
  * @m: the desired destination number
@@ -13,9 +13,10 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned long int count;
 
+	count = 0;
 	while (n != 0 || m != 0)
 	{
-		if ((n &1) != (m & 1))
+		if ((n & 1) != (m & 1))
 		{
 			count = count + 1;
 		}

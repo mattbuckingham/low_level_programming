daa06eb91e6666e2f0b9313224dd9b6d19763e8f
bigBadMatt <5707@holbertonstudents.com>
2022-11-27 03:43:15 -0800

    fixed issue when file already existed and contained text

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -4,41 +4,42 @@
 
 /**
  * append_text_to_file - function that appends text at the end of a file
- * @filename:
- * @text_content:
+ * @filename: name of the file
+ * @text_content: text to be inserted
  * Return: 1 on sucess, -1 on failure
 */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, wr;
+	int fd, wr, count;
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
 
-	if (text_content == NULL)
-	{
-		fd = open(filename, O_CREAT | O_WRONLY);
-		if (fd == -1)
-		{
-			return (-1);
-		}
-		return (1);
-	}
+	fd = open(filename, O_CREAT | O_WRONLY);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 	{
 		return (-1);
 	}
-	wr = write(fd, text_content, strlen(text_content));
-	if (wr == -1)
+
+	if (text_content == NULL)
 	{
-		close (fd);
+		close(fd);
 		return (1);
 	}
-	close (fd);
+	count = 0;
+	while (text_content[count] != '\0')
+	{
+		count = count + 1;
+	}
+	wr = write(fd, text_content, count);
+	if (wr < 1)
+	{
+		return (-1);
+	}
+	close(fd);
 	return (1);
 }
 

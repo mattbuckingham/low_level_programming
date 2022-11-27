84e3f16695c18d5389963b8c3f94de66b0328e41
bigBadMatt <5707@holbertonstudents.com>
2022-11-27 02:08:46 -0800

    fixed formatting to satisfy linter

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -3,9 +3,9 @@
 #include "hash_tables.h"
 
 /**
- *
- *
- *
+ * hash_table_create - function that creates a hash table.
+ * @size: the size of the array
+ * Return: the newly created table
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {

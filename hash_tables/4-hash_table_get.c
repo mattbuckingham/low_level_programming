65728d41bee7083ddc3d20b76cddede810f15eac
bigBadMatt <5707@holbertonstudents.com>
2022-11-27 02:47:04 -0800

    fixed formatting to satisfy linter

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -13,6 +13,11 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *element;
 
+	if(ht == NULL)
+	{
+		return (NULL);
+	}
+	
 	index = key_index((unsigned char *)key, ht->size);
 	element = ht->array[index];
 

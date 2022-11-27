de7be37521ab4126a505f927ecd68d2ebbcb6c18
bigBadMatt <5707@holbertonstudents.com>
2022-11-27 04:57:08 -0800

    fixed formatting to satisfy linter

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -13,42 +13,42 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	 char *buffer;
 	 int fd, rd, wr;
 
-	 if (filename == NULL)
-	 {
-		 return (0);
-	 }
+	if (filename == NULL)
+	{
+		return (0);
+	}
 
-	 buffer = malloc(sizeof(*buffer) * letters);
+	buffer = malloc(sizeof(*buffer) * letters);
 
-	 if (buffer == NULL)
-	 {
-		 return (0);
-	 }
+	if (buffer == NULL)
+	{
+		return (0);
+	}
 
-	 fd = open(filename, O_RDONLY);
-	 if (fd == -1)
-	 {
-		 free(buffer);
-		 return(0);
-	 }
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		free(buffer);
+		return(0);
+	}
 
-	 rd = read(fd, buffer, letters);
-	 if (rd == -1)
-	 {
-		 free(buffer);
-		 close(fd);
-		 return (0);
-	 }
+	rd = read(fd, buffer, letters);
+	if (rd == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 
-	 wr = write(STDOUT_FILENO, buffer, rd);
-	 if (wr == -1)
-	 {
-		 free(buffer);
-		 close(fd);
-		 return (0);
-	 }
+	wr = write(STDOUT_FILENO, buffer, rd);
+	if (wr == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 
-	 free(buffer);
-	 close(fd);
-	 return (wr);
+	free(buffer);
+	close(fd);
+	return (wr);
 }

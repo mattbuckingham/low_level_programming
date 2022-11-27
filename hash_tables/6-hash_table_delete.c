f479d472e3cc02df5ab03b1c33f9be280c1e313d
bigBadMatt <5707@holbertonstudents.com>
2022-11-27 03:20:44 -0800

    deletes a hash table

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: a hash table
+ *
+*/
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t nxt, tmp;
+	unsigned int i;
+
+	i = 0;
+
+	if (ht == NULL)
+	{
+		return;
+	}
+
+	while (i < ht->size)
+	{
+		nxt = ht->array[i];
+		while (nxt != NULL)
+		{
+			nxt = tmp;
+			nxt = nxt->next;
+			free(tmp->key);
+			free(tmp->value);
+			free(tmp);
+		}
+		i = i + 1;
+	}
+	free(ht->array);
+	free(ht);
+}
